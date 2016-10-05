c1978f92f01aa74a19ea9fc53197d1c8369898f6
MinewayAltis <18silkstonecj1@webmail.sis.edu.hk>
2016-10-05 09:55:14 +0800

    game test lolz

---
+++ test123/js/index.js
@@ -0,0 +1,37 @@
+function quiz() {
+  var question1 = prompt("What is the capital of China?")
+  if (question1 === "Beijing") {
+    alert("Correct!")
+  }
+  else {
+    alert("nope!")
+  }
+  var question2 = prompt("What is the capital of Thailand?")
+  if (question2 === "Bangkok") {
+    alert("Correct!")
+  }
+  else {
+    alert ("nope!")
+  }
+  var question3 = prompt("What is the capital of Oman?")
+  if (question3 === "Muscat") {
+    alert("Quorekt!")
+  }
+  else {
+    alert("knopee!")
+  }
+  var question4 = prompt("What is the capital of Australia?")
+  if (question4 === "Canberra") {
+    alert("Correct!")
+  }
+  else {
+    alert("nope!")
+  }
+  var question5 = prompt("What is the capital of Laos")
+  if (question5 === "Vientiane") {
+    alert("Correct!")
+  }
+  else {
+    alert("nope!")
+  }
+}
